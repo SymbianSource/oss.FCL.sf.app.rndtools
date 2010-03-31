8ff2d9130002f02fa1c27b492748f0c643307c3a
Dremov Kirill (Nokia-D-MSW/Tampere) <kirill.dremov@nokia.com>
2010-03-31 21:32:05 +0300

    Revision: 201011 Kit: 201013
    --HG--
    branch : RCL_3

--- stifui/stifui/inc/version.h
+++ stifui/stifui/inc/version.h
@@ -20,9 +20,9 @@
 
 #define STIFUI_MAJOR_VERSION 7
 #define STIFUI_MINOR_VERSION 3
-#define STIFUI_BUILD_VERSION 27
+#define STIFUI_BUILD_VERSION 28
 
-#define STIFUI_REL_DATE "23th Feb 2010"
+#define STIFUI_REL_DATE "09th Mar 2010"
 
 #define TO_UNICODE(text) _L(text) 
 
