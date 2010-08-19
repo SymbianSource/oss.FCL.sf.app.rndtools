eb52b5a94043b7768f444e394a11ba885f99d2a8
Dremov Kirill (Nokia-D-MSW/Tampere) <kirill.dremov@nokia.com>
2010-08-19 09:55:38 +0300

    Revision: 201029 Kit: 201033
    --HG--
    branch : RCL_3

--- loadgen/inc/loadgen_netconn.h
+++ loadgen/inc/loadgen_netconn.h
@@ -22,7 +22,7 @@
 // INCLUDES
 #include <e32std.h>
 #include <e32base.h>
-#include <DownloadMgrClient.h> 
+#include <downloadmgrclient.h>
 
 #include "loadgen_loadbase.h"
 #include "loadgen_loadattributes.h"
